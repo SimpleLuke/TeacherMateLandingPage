ee099094f0fde52d6b9a2576d17e452645465391
Luke Lai <lukelai.dev@gmail.com>
2024-02-24 11:51:27 +0000

    Update content

--- src/EmailList.jsx
+++ src/EmailList.jsx
@@ -7,7 +7,7 @@ export default function EmailList() {
       <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="relative isolate overflow-hidden bg-gray-900 px-6 py-24 shadow-2xl sm:rounded-3xl sm:px-24 xl:py-32">
           <h2 className="mx-auto max-w-2xl text-center text-3xl font-bold tracking-tight text-white sm:text-4xl">
-	  Stay tuned and get access to our test service:
+	  Stay tuned and get access to our test service
           </h2>
           <p className="mx-auto mt-2 max-w-xl text-center text-lg leading-8 text-gray-300">
 			If you would like to test out our product and get access to it for free during the testing period leave us you’re email and well be in touch!
